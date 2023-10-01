24b653cc8886ed0dee79f70c162bf8619cdf717a
chris <davischris25139@yahoo.com>
2023-10-01 00:23:21 -0700

    added const values to the script

--- assets/script.js
+++ assets/script.js
@@ -15,12 +15,35 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
+var choices = ['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60']
 
-
+const length = (userChoice)
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "!@#$%^&"
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+    
+function pwgen() {
+    let userChoice = prompt('Select amount of character in password 8-60')
+
+    if (!userChoice) {
+        return
+    }
+
+    while (
+        !choices.includes(userChoice)
+    ) {
+        alert('You must choose a number between 8-60')
+        userChoice = prompt('Try again! Choose a number between 8-60!')
+    }
+
+    alert('You have chosen ' + userChoice)
+    console.log(length)
+}
 
 // Write password to the #password input
 function writePassword() {
