67804cf8278802881467e00667234033843a7097
chris <davischris25139@yahoo.com>
2023-10-02 12:50:19 -0700

    added commits to each line of functions to explain how they work

--- assets/script.js
+++ assets/script.js
@@ -14,27 +14,28 @@ WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
-
-var choices = ['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60']
 */
-
-    //length = (min="8", max="60")
-    
-    
     // Assignment Code
+    // This defines generateBtn and tells the script where to find it on the html
     var generateBtn = document.querySelector('#generate')
     
-    
+    // this defines all the arrays that my randomFunc() can process
     const upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O' , 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     const lowerCaseArr = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     const numbersArr = [0,1,2,3,4,5,6,7,8,9]
     const specialCharactersArr = ['!','@','#','$','%','^','&']
     
-
-function randomfunc() {
+// this will contain the logic for processing user input as well as how the computer will take in the criteria.
+function randomFunc() {
+    // var password = [] creates a blank array which the random math generator will .push into
     var password = []
+    // allCharactersArr = [] creates a blank array which the user input and computer logic will add the defined arrays above into for processing in the mathrandom
     var allCharactersArr = []
     
+
+    // var characters, upperCase, lowerCase, numbers, specialCharacters all are prompts or confirms to retrieve logic from the user
+    // characters prompt returns a definitive number within its range otherwise returns the user to the start of randomFunc()
+
     var characters = prompt('Select amount of characters in password 8-128')
     if (characters >= 8 && characters <= 128) {
         console.log("length: " + length);
@@ -42,53 +43,57 @@ function randomfunc() {
     } else {
         var length = false;
         alert("You must choose a character number of 8-128");
-        return randomfunc()
+        return randomFunc()
     }
     
-    
-var upperCase = confirm('Do you want to use Uppercase letters?')
-    if (upperCase === true){
-        alert('Uppercase will be used.')
-        console.log('uppercase use ' + upperCase)
-    } else {
-        alert('Uppercase will not be used.')
-        console.log('uppercase use ' + upperCase)
-    }
-    
-    
-    
-var lowerCase = confirm('Do you want to use Lowercase letters?')
-    if (lowerCase === true){
-        alert('Lowercase will be used.')
-        console.log('lowercase use ' + lowerCase)
-    } else {
-        alert('Lowercase will not be used.')
-        console.log('lowercase use ' + lowerCase)
-    }
-    
-    
-    
-var numbers = confirm('Do you want to use Numbers?')
-    if (numbers === true){
-        alert('Numbers will be used.')
-        console.log('numbers use ' + numbers)
-    } else {
-        alert('Numbers will not be used.')
-        console.log('numbers use ' + numbers)
-    }
-    
-    
-    
-    
-var specialCharacters = confirm('Do you want to use Special Characters?')
-    if (specialCharacters === true){
-            alert('Special Characters will be used.')
-            console.log('specialCharacters use ' + specialCharacters)
+    // upperCase, lowerCase, numbers, specialCharacters all are confirms which return true or false booleans
+    // the true or false information is then logged into the console as well as alerting the user of their selection
+
+    var upperCase = confirm('Do you want to use Uppercase letters?')
+        if (upperCase === true){
+            alert('Uppercase will be used.')
+            console.log('uppercase use ' + upperCase)
         } else {
-            alert('Special Characters will not be used.')
-            console.log('specialCharacters use ' + specialCharacters)
+            alert('Uppercase will not be used.')
+            console.log('uppercase use ' + upperCase)
         }
     
+    
+    
+    var lowerCase = confirm('Do you want to use Lowercase letters?')
+        if (lowerCase === true){
+            alert('Lowercase will be used.')
+            console.log('lowercase use ' + lowerCase)
+        } else {
+            alert('Lowercase will not be used.')
+            console.log('lowercase use ' + lowerCase)
+        }
+    
+    
+    
+    var numbers = confirm('Do you want to use Numbers?')
+        if (numbers === true){
+            alert('Numbers will be used.')
+            console.log('numbers use ' + numbers)
+        } else {
+            alert('Numbers will not be used.')
+            console.log('numbers use ' + numbers)
+        }
+    
+    
+
+    var specialCharacters = confirm('Do you want to use Special Characters?')
+        if (specialCharacters === true){
+                alert('Special Characters will be used.')
+                console.log('specialCharacters use ' + specialCharacters)
+            } else {
+                alert('Special Characters will not be used.')
+                console.log('specialCharacters use ' + specialCharacters)
+            }
+    
+
+    // these if statements will pull from the above respective elements and if the statement is true it add the respective elements array to allCharactersArr
+
     if (upperCase === true) {
          allCharactersArr = allCharactersArr.concat (upperCaseArr)         
         }
@@ -103,6 +108,12 @@ var specialCharacters = confirm('Do you want to use Special Characters?')
        }
        console.log(allCharactersArr)
        
+
+    // this for loop is stating i < characters meaning whatever number the user input for prompt characters above will be used as its number to stop at.
+    // var defines what the randomArrMath will be
+    // password.push will pull 1 letter from the allCharactersArr that has been processed by randomArrMath amd add it to the password [] array above
+    // this process will continue to loop until i = characters
+    // return password.join('') returns the newly generated array of text to the passwordText.value = password below but as a string instead of an array
        
        for (i = 0; i < characters; i++) {
            var randomArrMath = Math.floor(Math.random()* allCharactersArr.length)
@@ -113,12 +124,17 @@ var specialCharacters = confirm('Do you want to use Special Characters?')
     
     
     // Write password to the #password input
+    //  writePassword is calling randomFunc
+    // it is also stating that the passwordText will find #password
+    // passwordText.value = password means that whatever password comes back as will now be passwordText which will apply to the html file under #password
     function writePassword() {
-        var password = randomfunc()
+        var password = randomFunc()
         var passwordText = document.querySelector("#password");
         passwordText.value = password;
     }
     
     // Add event listener to generate button
+    //This will make the button reactive to click and call writePassword()
+    // but generateBtn must be defined above for it to be responsive
     generateBtn.addEventListener("click", writePassword);
     
